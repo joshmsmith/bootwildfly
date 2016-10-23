1d39cd9b677711ad3e99afaba852285966aaa26d
heywoodbanks <joe.zehnder@gmail.com>
2016-10-23 18:11:10 -0400

    Attempting to display computer name in hello servlet

--- src/main/java/bootwildfly/HelloWildFlyController.java
+++ src/main/java/bootwildfly/HelloWildFlyController.java
@@ -3,12 +3,24 @@ package bootwildfly;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 public class HelloWildFlyController {
 
 
     @RequestMapping("hello")
-    public String sayHello(){
-        return ("Hello, SpringBoot on Wildfly");
+    public String sayHello() {
+        return ("Hello, SpringBoot on Wildfly from computer: " + getComputerName());
+    }
+
+    private String getComputerName() {
+        Map<String, String> env = System.getenv();
+        if (env.containsKey("COMPUTERNAME"))
+            return env.get("COMPUTERNAME");
+        else if (env.containsKey("HOSTNAME"))
+            return env.get("HOSTNAME");
+        else
+            return "Unknown Computer";
     }
 }
